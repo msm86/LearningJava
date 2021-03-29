6b7866f2417c31252af9e8bd1a858bcc4f07286b
mital <mital@192.168.0.135>
2021-03-29 17:01:12 +0200

    First Commit for Check Packages

---
+++ CheckPackages/src/Main.java
@@ -0,0 +1,12 @@
+import com.example.PackageChallenge.Series;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for(int i = 0; i<=10; i++) {
+			System.out.println(Series.nSum(i));
+		}
+
+	}
+
+}
